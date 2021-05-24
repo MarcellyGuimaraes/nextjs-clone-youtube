27706fb83a8ef28e789c9ff5c12f2e2c4ecb98fe
fotofacil <fotofacil@live.com>
2021-05-24 17:40:44 -0300

    Nextjs Clone Youtube

---
+++ src/components/Layout/TopBar.js
@@ -0,0 +1,143 @@
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Box from '@material-ui/core/Box';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import {
+  Button,
+  Hidden,
+  IconButton,
+  InputBase,
+  Paper,
+} from '@material-ui/core';
+import {
+  AccountCircle,
+  Apps,
+  Brightness4,
+  Brightness7,
+  MoreVert,
+  VideoCall,
+} from '@material-ui/icons';
+import { signIn, useSession, signOut } from 'next-auth/client';
+import useSettings from 'src/hooks/useSettings';
+import THEMES from 'src/utils/constants';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    boxShadow: 'none',
+    zIndex: theme.zIndex.drawer + 1,
+    backgroundColor: theme.palette.background.default,
+  },
+  toolbar: {
+    minHeight: 56,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  logo: {
+    cursor: 'pointer',
+    height: 18,
+    marginLeft: theme.spacing(3),
+  },
+  search: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    height: 35,
+    width: 700,
+  },
+  input: {
+    flex: 1,
+  },
+}));
+
+function TopBar() {
+  const classes = useStyles();
+  const [session] = useSession();
+  const { settings, saveSettings } = useSettings();
+  return (
+    <AppBar className={classes.root} color="default">
+      <Toolbar className={classes.toolbar}>
+        <Box display="flex" alignItems="center">
+          <MenuIcon />
+          <img
+            src={
+              settings.theme === THEMES.DARK
+                ? '/branco.png'
+                : '/new-youtube-logo.svg'
+            }
+            alt="logo"
+            className={classes.logo}
+          />
+        </Box>
+
+        <Hidden mdDown>
+          <Box>
+            <Paper component="form" className={classes.search}>
+              <InputBase
+                className={classes.input}
+                placeholder="Pesquisar"
+                inputProps={{ 'aria-label': 'search google maps' }}
+              />
+
+              <IconButton type="submit" aria-label="search">
+                <SearchIcon />
+              </IconButton>
+            </Paper>
+          </Box>
+        </Hidden>
+
+        <Box display="flex">
+          <IconButton className={classes.icons}>
+            {settings.theme === THEMES.DARK ? (
+              <Brightness7
+                onClick={() => saveSettings({ theme: THEMES.LIGHT })}
+              />
+            ) : (
+              <Brightness4
+                onClick={() => saveSettings({ theme: THEMES.DARK })}
+              />
+            )}
+          </IconButton>
+
+          <IconButton className={classes.icons}>
+            <VideoCall />
+          </IconButton>
+
+          <IconButton className={classes.icons}>
+            <Apps />
+          </IconButton>
+
+          <IconButton className={classes.icons}>
+            <MoreVert />
+          </IconButton>
+
+          {!session ? (
+            <Button
+              color="secondary"
+              component="a"
+              variant="outlined"
+              startIcon={<AccountCircle />}
+              onClick={() => signIn('google')}
+            >
+              Fazer Login
+            </Button>
+          ) : (
+            <Box display="flex" alignItems="center">
+              <Avatar
+                onClick={() => signOut()}
+                alt="User"
+                className={classes.avatar}
+                src={session?.user?.image}
+              />
+            </Box>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default TopBar;
---
+++ src/database/getVideos.js
@@ -0,0 +1,44 @@
+export async function getVideos() {
+  const datas = [
+    {
+      id: 1,
+      title:
+        'FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29',
+      authorId: 1,
+      authorName: 'Marcelly Guimarães',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: 'thumbs/next01.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      title:
+        'FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29',
+      authorId: 1,
+      authorName: 'Marcelly Guimarães',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: 'thumbs/next02.jpg',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+    {
+      id: 3,
+      title:
+        'FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29',
+      authorId: 1,
+      authorName: 'Marcelly Guimarães',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: 'thumbs/next03.png',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
+  ];
+
+  return datas;
+}
+
+export default getVideos;
