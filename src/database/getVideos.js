61e97e38f651b511eeff90ae39cce3c06988c625
fotofacil <fotofacil@live.com>
2021-05-24 20:13:35 -0300

    nextjs-clone-youtube

--- src/database/getVideos.js
+++ src/database/getVideos.js
@@ -36,6 +36,18 @@ export async function getVideos() {
       videoUrl: 'url',
       updatedAt: new Date(),
     },
+    {
+      id: 4,
+      title:
+        'FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29',
+      authorId: 1,
+      authorName: 'Marcelly Guimarães',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: 'thumbs/next04.jpg',
+      videoUrl: 'url',
+      updatedAt: new Date(),
+    },
   ];
 
   return datas;
